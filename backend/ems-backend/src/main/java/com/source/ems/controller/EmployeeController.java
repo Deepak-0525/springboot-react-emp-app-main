0eafe4505f4c0042d08bfb8d270cde4fb8628b7b
Deepak-0525 <deepaksaravanan45@gmail.com>
2025-05-23 13:10:43 +0530

    update

--- backend/ems-backend/src/main/java/com/source/ems/controller/EmployeeController.java
+++ backend/ems-backend/src/main/java/com/source/ems/controller/EmployeeController.java
@@ -1,11 +1,14 @@
 package com.source.ems.controller;
 
 import com.source.ems.dto.EmployeeDto;
+import com.source.ems.entity.Employee;
 import com.source.ems.service.EmployeeService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @AllArgsConstructor
 @RestController
@@ -26,6 +29,28 @@ public class EmployeeController {
     public ResponseEntity<EmployeeDto> getEmployeeById(@PathVariable("id") Long employeeId){
         EmployeeDto employeeDto = employeeService.getEmployeeById(employeeId);
         return ResponseEntity.ok(employeeDto);
+
+    }
+
+    // Build get All Employee REST API
+    @GetMapping
+    public ResponseEntity<List<EmployeeDto>> getAllEmployees(){
+        List<EmployeeDto> employees = employeeService.getAllEmployees();
+        return ResponseEntity.ok(employees);
+    }
+
+    // Build Update Employee REST API
+    @PutMapping("{id}")
+    public ResponseEntity<EmployeeDto> updateEmployee(@PathVariable("id") Long employeeId, @RequestBody EmployeeDto updatedEmployee){
+        EmployeeDto employeeDto = employeeService.updateEmployee(employeeId, updatedEmployee);
+        return ResponseEntity.ok(employeeDto);
+    }
+
+    // Build Delete Employee REST API
+    @DeleteMapping("{id}")
+    public ResponseEntity<String> deleteEmployee(@PathVariable("id") Long employeeId){
+        employeeService.deleteEmployee(employeeId);
+        return ResponseEntity.ok("Employee Deleted Successfully..!");
 
     }
 
--- backend/ems-backend/src/main/java/com/source/ems/service/EmployeeService.java
+++ backend/ems-backend/src/main/java/com/source/ems/service/EmployeeService.java
@@ -2,8 +2,16 @@ package com.source.ems.service;
 
 import com.source.ems.dto.EmployeeDto;
 
+import java.util.List;
+
 public interface EmployeeService {
     EmployeeDto createEmployee(EmployeeDto employeeDto);
     
     EmployeeDto getEmployeeById(Long employeeId);
+
+    List<EmployeeDto> getAllEmployees();
+
+    EmployeeDto updateEmployee(Long employeeId, EmployeeDto updatedEmployee);
+
+    void deleteEmployee(Long employeeId);
 }
